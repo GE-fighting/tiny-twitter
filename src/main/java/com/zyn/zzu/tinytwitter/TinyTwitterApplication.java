977d2f44eb736a4b2b0597534dfbd110f554217b
章俊 <1501790343@qq.com>
2022-11-01 19:38:50 +0800

    commit main

---
+++ src/main/java/com/zyn/zzu/tinytwitter/TinyTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.zyn.zzu.tinytwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyTwitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyTwitterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyn/zzu/tinytwitter/TinyTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyn.zzu.tinytwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyTwitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
